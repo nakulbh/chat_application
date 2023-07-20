ad98f1ba7d4a398153258cee5bdb48ec241096af
Nakul <nakulbhardwaj37@gmail.com>
2023-07-20 20:34:22 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      {
+        headers: {
+          "private-key": "48be7fd3-fb9c-443f-8e69-1a080e030701",
+        }
+      }
+    );
+    return res.status(r.status).json(r.data);
+
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3000);
